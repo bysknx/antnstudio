e957629ba670461c350303b4b8b912cf78ca3679
sk <107989373+bysknx@users.noreply.github.com>
2025-10-09 18:28:43 +0200

    Update LoadingAscii.tsx

--- components/LoadingAscii.tsx
+++ components/LoadingAscii.tsx
@@ -43,7 +43,7 @@ export default function LoadingAscii() {
       try {
         localStorage.setItem(STORAGE_KEY, String(Date.now()));
       } catch {}
-    }, 1400); // durée totale d’apparition
+    }, 5000); // durée totale d’apparition
 
     return () => {
       if (timerRef.current) window.clearTimeout(timerRef.current);
