632e050a68f3bfac059b059e46a4ecb6c6b1e649
sk <107989373+bysknx@users.noreply.github.com>
2025-10-13 00:26:51 +0200

    Update FooterMount.tsx

--- components/FooterMount.tsx
+++ components/FooterMount.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import FooterPen from "./FooterPen";
+import dynamic from "next/dynamic";
+const FooterFromPen = dynamic(() => import("./FooterFromPen"), { ssr: false });
 
-/**
- * Affiche le footer global partout SAUF sur /projects
- * (l’iframe possède déjà son propre footer).
- */
 export default function FooterMount() {
-  const pathname = usePathname();
-  if (pathname?.startsWith("/projects")) return null;
-  return <FooterPen />;
+  return <FooterFromPen />;
 }
