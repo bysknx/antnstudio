661f688de1394f823ea4c79be8f59ac8454bd58c
sk <107989373+bysknx@users.noreply.github.com>
2025-10-13 22:43:04 +0200

    Create PrewarmVimeo.tsx

---
+++ components/PrewarmVimeo.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { useEffect } from "react";
+export default function PrewarmVimeo(){
+  useEffect(()=>{
+    fetch("/api/vimeo", { cache: "no-store" }).catch(()=>{});
+  },[]);
+  return null;
+}
