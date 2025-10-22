c850dc263fc1d8ceb256a3c4b25a99563656927d
sk <lesknx@gmail.com>
2025-10-22 16:38:11 +0200

    Update de con

--- components/PrewarmVimeo.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useEffect } from "react";
-export default function PrewarmVimeo() {
-  useEffect(() => {
-    fetch("/api/vimeo", { cache: "no-store" }).catch(() => {});
-  }, []);
-  return null;
-}
